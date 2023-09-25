6c3b4fbceae489b34d57cbe8c4d1f1a397dff280
root <root@Rafael-Merlotto>
2023-09-25 21:59:04 +0200

    add token

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,74 @@
 import express from 'express'
 import { prisma } from './prisma'
+import { User } from '@prisma/client'
+import { compareSync, hashSync } from 'bcrypt'
+import jwt from 'jsonwebtoken';
+import { getJwtKeys } from './key';
 
 const server = express()
 server.use(express.json())
 
 
+async function verifyUser(email: string, password: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+        where: {
+            email: email
+        }
+    })
+    if (!user) {
+        return null
+    }
+    if (!compareSync(password, user.password)) {
+        return null
+    }
+    return user
+}
+
+
+async function generateJwt(user: User): Promise<string> {
+    const payload = {
+        aud: 'access',
+        id: user.id,
+        email: user.email
+    }
+    const { privateKey } = await getJwtKeys();
+    return jwt.sign(payload, privateKey, { algorithm: 'RS256' })
+}
+
 server.post('/login', async (req, res) => {
-    const body = req.body
-    try {
-        const user = await prisma.user.findUnique({
-            where: {
-                email: body.email
-            }
-        })
-        res.status(201).send(`Hello ${user?.name}`)
-    }catch{
+    const { email, password } = req.body
+    const user = await verifyUser(email, password)
+    if (!user) {
         res.status(403).send({ msg: 'Invalid authentication' })
     }
+    const token = await generateJwt(user!);
+    return res.status(201).send({
+        accessToken: token
+    })
+})
+
+
+server.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    const passwordHash = hashSync(password, 5);
+    let user: User;
+    try {
+        user = await prisma.user.create({
+            data: {
+                email: email,
+                name: name,
+                password: passwordHash
+            }
+        })
+        return res.status(201).send({
+            id: user.id,
+            email: user.email,
+            name: user.name
+        })
+    } catch {
+        return res.status(401).send({ msg: 'cannot create user' })
+    }
+
 })
 
 server.get('/', async (req, res) => {
